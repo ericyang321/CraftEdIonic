7e17ec6cf6a1e86bce409961235dceb99c1341f7
Brigette O'Shaughnessy <email": null>
2016-10-03 14:15:09 -0700

    provide rollback point

--- www/js/controllers/FlavorController.js
+++ www/js/controllers/FlavorController.js
@@ -1,7 +1,25 @@
-craftEd.controller('FlavorController', ['$scope', '$http', function($scope, $http){
-    $http.get('http://how-to-train-your-palate-api.herokuapp.com/users/:id/flavors/new')
-    .then(function(response){
-      console.log(response);
+craftEd.controller('FlavorController', ['auth', '$scope', '$http', function($scope, $http){
+
+  function setHeader() {
+    return {
+
+    };
+  };
+
+  var tokens = {
+    headers: {
+      "access-token": window.sessionStorage.token,
+      "token-type": "Bearer",
+      "client": window.sessionStorage.client,
+      "expiry": window.sessionStorage.expiry,
+      "uid": window.sessionStorage.uid
+    }
+  };
+
+    console.log(tokens)
+     $http.get('http://how-to-train-your-palate-api.herokuapp.com/users/:user_id/flavors/new', tokens)
+     .then(function(response){
+      // console.log(response);
       $scope.allFlavors = response.data
-    });
+      });
 }]);
--- www/js/controllers/UserController.js
+++ www/js/controllers/UserController.js
@@ -1,33 +1,19 @@
 var config = {
   headers: {
-        'content-type': 'application/json'
-      }
+    'content-type': 'application/json'
+  }
+};
+
+
+craftEd.controller('UserController', ['auth', '$scope', '$http', '$location', '$ionicPopup',function(auth, $scope, $http, $location, $ionicPopup){
+
+  function storeSession(response) {
+    window.sessionStorage.token = response.headers('access-token');
+    window.sessionStorage.client = response.headers('client');
+    window.sessionStorage.expiry = response.headers('expiry');
+    window.sessionStorage.uid = response.headers('uid')
+
   };
-
-
-craftEd.controller('UserController', ['$scope', '$http', '$location', '$ionicPopup', function($scope, $http, $location, $ionicPopup){
-
-  // function setHeader() {
-  //   return {
-  //     "access-token": window.sessionStorage.token,
-  //     "token-type": "Bearer",
-  //     "client": window.sessionStorage.client,
-  //     "expiry": window.sessionStorage.expiry,
-  //     "uid": window.sessionStorage.uid
-  //   };
-  // };
-
-  // function storeSession(response, setUser) {
-  //   window.sessionStorage.token = response.headers('access-token');
-  //   window.sessionStorage.client = response.headers('client');
-  //   window.sessionStorage.expiry = response.headers('expiry');
-  //   window.sessionStorage.uid = response.headers('uid');
-  //   window.sessionStorage.email = setUser.email;
-  //   window.sessionStorage.first_name = setUser.first_name;
-  //   window.sessionStorage.last_name = setUser.last_name;
-  //   window.sessionStorage.nickname = setUser.nickname;
-  //   window.sessionStorage.user_id = setUser.id;
-  // };
 
   showAlert = function(alert) {
     var alertPopup = $ionicPopup.alert({
@@ -51,26 +37,17 @@ craftEd.controller('UserController', ['$scope', '$http', '$location', '$ionicPop
 
   $scope.login = function(){
     login = {email: $scope.email, password: $scope.password};
-    $http.post(rootUrl +'/v1/auth/sign_in', login, config);
-    $location.path('#/profile');
+    $http.post(rootUrl +'/v1/auth/sign_in', login, config)
+      .then(function(response){
+        globalResponse = response
+        console.log(response.data)
+        console.log()
+        storeSession(response);
+      })
+    $location.path('#/home');
   }
 
-  //   if($scope.email && $scope.password) {
-  //     authentication.authenticate($scope.email, $scope.password)
-  //       .then(function(response) {
-  //         storeSession(response, response.data.data);
-  //         $state.go('home');
-  //       })
-  //       .catch(function(data) {
-  //         showAlert(data.data.errors[0]);
-  //       });
-  //   }
-  //   else {
-  //     showAlert("Invalid Login");
-  //   }
-  // };
-
-  // $scope.logout = function() {
-  //   window.sessionStorage.clear();
-  // };
+  $scope.logout = function() {
+    window.sessionStorage.clear();
+  };
 }]);
