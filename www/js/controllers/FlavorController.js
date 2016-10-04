a2a1d39a2fb08ce17de02ad13dcb8a2d0b61b271
Joseph Frank <email": null>
2016-10-03 18:07:18 -0700

    Update flavor/ratings/recs templates/controllers

--- www/js/controllers/FlavorController.js
+++ www/js/controllers/FlavorController.js
@@ -1,5 +1,5 @@
 craftEd.controller('FlavorController', ['$scope', '$http', function($scope, $http){
-    $http.get('http://how-to-train-your-palate-api.herokuapp.com/users/:id/flavors/new')
+  $http.get('http://how-to-train-your-palate-api.herokuapp.com/users/:id/flavors/new')
     .then(function(response){
       console.log(response);
       $scope.allFlavors = response.data
