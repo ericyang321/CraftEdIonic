27fcede2cc7552499ee7ef52dd892ef116cbaa66
John Gill <j.gill0431@gmail.com>
2016-10-07 11:40:42 -0700

    added profile controller.

---
+++ www/js/controllers/ProfileController.js
@@ -0,0 +1,8 @@
+craftEd.controller('ProfileController', ['$scope', '$http', '$location', '$ionicPopup', '$window',function($scope, $http, $location, $window, $ionicPopup){
+// $scope.reload();
+$scope.$on("$ionicView.afterEnter", function(event, data){
+  console.log("hello")
+})
+
+
+}]);
